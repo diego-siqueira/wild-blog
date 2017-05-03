08bf70aa511ab538d29806548f3a2a024c2aa574
diego-siqueira <diego_sp_br@hotmail.com>
2017-05-03 23:50:18 +0200

    Function improvement working well + Fix link of algos added to menu

--- public/js/components/algos/algo2/algo2.js
+++ public/js/components/algos/algo2/algo2.js
@@ -1,10 +1,22 @@
-
 let algo2 = {
   templateUrl: 'js/components/algos/algo2/algo2.tp.html',
   controller: ['$stateParams', '$state', function($stateParams, $state) {
     'use  strict'
 
     this.numberTest = 14928418679754190000
+
+    let times = 0
+    let paper = 0.0001
+
+    function foldTo(distance) {
+      if (distance <= 0) {
+        return null
+      }
+      while (paper < distance) {
+        paper = paper * 2
+        times++
+      }
+    }
 
     foldTo(this.numberTest);
     this.timesFold = times
